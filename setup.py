83d0aa3c5400c16345b1b9835c2cc634748f71b2
gfixler <gary@garyfixler.com>
2016-10-12 16:05:05 -0700

    Add barebones setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='fmaya',
+      version='0.1',
+      description='Functional expressions for Autodesk Maya.',
+      url='https://github.com/gfixler/fmaya',
+      author='Gary Fixler',
+      author_email='gfixler@gmail.com',
+      license='GNU GPLv3',
+      packages=['fmaya'],
+      zip_safe=False)
+
