69212778add79fd20dda34a638f12ce919d6d677
gfixler <gary@garyfixler.com>
2016-11-10 15:51:39 -0800

    Denote pure/impure sections in core/chan.py

--- fmaya/core/chan.py
+++ fmaya/core/chan.py
@@ -1,7 +1,12 @@
+# PURE
+
 nodeFromChannel = lambda channel: channel.split('.')[0]
 attrFromChannel = lambda channel: channel.split('.')[-1]
 chanFromChannel = lambda channel: (nodeFromChannel(channel), attrFromChannel(channel))
 attrToChannel = lambda node: preadd(node + ".")
+
+
+# IMPURE
 
 getChannelAtTime = lambda time: lambda channel: cmds.getAttr(channel, time=time)
 getChannel = getChannelAtTime(cmds.currentTime(query=True))
