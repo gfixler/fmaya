75fb37f1daf7fda380216915e3c50479524b3162
gfixler <gary@garyfixler.com>
2016-11-10 15:32:27 -0800

    Add scenePath/sceneName functions to core/scene.py

--- fmaya/core/scene.py
+++ fmaya/core/scene.py
@@ -3,3 +3,6 @@ from string import digits, ascii_letters
 validNameChars = digits + ascii_letters
 toValidMayaName = lambda name: ''.join(cmap(lambda x: x if x in validNameChars else '_')(name))
 
+scenePath = lambda: cmds.file(query=True, sceneName=True)
+sceneName = lambda: scenePath().split('/')[-1]
+
