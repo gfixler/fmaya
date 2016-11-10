003e572f29d6bddb096b21ea9ed81ae5047eb740
gfixler <gary@garyfixler.com>
2016-11-10 14:48:59 -0800

    Add comp and const functions to core/pure.py

--- fmaya/core/pure.py
+++ fmaya/core/pure.py
@@ -1,5 +1,7 @@
 ident = lambda x: x
 const = lambda x: lambda _: x
+comp = lambda *fs: reduce(lambda f, g: lambda x: f(g(x)), fs, ident)
+cmap = lambda f: lambda xs: map(f, xs)
 
 curry = lambda f: lambda x: lambda y: f(x, y)
 uncurry = lambda f: lambda x, y: f(x)(y)
