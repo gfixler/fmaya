2c32c6d4434ff4ea2e0dce638acceacb4f830702
gfixler <gary@garyfixler.com>
2016-11-10 17:02:01 -0800

    Fix selection1 function in core/select.py

--- fmaya/core/select.py
+++ fmaya/core/select.py
@@ -3,7 +3,7 @@ import maya.mel as mel
 
 
 selection = lambda: (lambda xs: xs if xs else [])(cmds.ls(selection=True, flatten=True))
-selection1 = lambda: sel()[0] # non-total; errors on empty selection
+selection1 = lambda: selection()[0] # non-total; errors on empty selection
 
 channelBoxSelection = lambda: mel.eval("channelBox -query -selectedMainAttributes $gChannelBoxName;")
 
