5c5785fc6b86c7e99146cc515029d7c6d6457667
gfixler <gary@garyfixler.com>
2016-11-10 16:57:10 -0800

    Add channelBoxSelection function to core/select.py

--- fmaya/core/select.py
+++ fmaya/core/select.py
@@ -1,6 +1,9 @@
 import maya.cmds as cmds
+import maya.mel as mel
 
 
 selection = lambda: (lambda xs: xs if xs else [])(cmds.ls(selection=True, flatten=True))
 selection1 = lambda: sel()[0] # non-total; errors on empty selection
+
+channelBoxSelection = lambda: mel.eval("channelBox -query -selectedMainAttributes $gChannelBoxName;")
 
