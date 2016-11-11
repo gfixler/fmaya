b8517f7ffb64a6252a1538c4d07af5ae2d7a18a7
gfixler <gary@garyfixler.com>
2016-11-10 17:10:13 -0800

    Fix imports in core/scene.py

--- fmaya/core/scene.py
+++ fmaya/core/scene.py
@@ -2,6 +2,8 @@ from string import digits, ascii_letters
 
 import maya.cmds as cmds
 
+from .pure import cmap
+
 
 validNameChars = digits + ascii_letters
 toValidMayaName = lambda name: ''.join(cmap(lambda x: x if x in validNameChars else '_')(name))
