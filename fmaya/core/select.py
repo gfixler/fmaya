4378c73614962b5479320f26397fb574d525a8a2
gfixler <gary@garyfixler.com>
2016-11-10 16:56:37 -0800

    Add missing import to core/select.py

--- fmaya/core/select.py
+++ fmaya/core/select.py
@@ -1,3 +1,6 @@
+import maya.cmds as cmds
+
+
 selection = lambda: (lambda xs: xs if xs else [])(cmds.ls(selection=True, flatten=True))
 selection1 = lambda: sel()[0] # non-total; errors on empty selection
 
