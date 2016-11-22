02c39740cdf175ef4db14f91a0ece3e069c8c282
gfixler <gary@garyfixler.com>
2016-11-21 17:48:28 -0800

    Fix arguments in pure.uncurry
    I was taking `x, y`, but needed to take (x, y).

--- fmaya/core/pure.py
+++ fmaya/core/pure.py
@@ -4,7 +4,7 @@ comp = lambda *fs: reduce(lambda f, g: lambda x: f(g(x)), fs, ident)
 cmap = lambda f: lambda xs: map(f, xs)
 
 curry = lambda f: lambda x: lambda y: f(x, y)
-uncurry = lambda f: lambda x, y: f(x)(y)
+uncurry = lambda f: lambda (x, y): f(x)(y)
 
 fst = lambda (x, _): x
 snd = lambda (_, y): y
