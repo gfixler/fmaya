965d59d925902e3834e7aa28c0126d67eb26f98b
gfixler <gary@garyfixler.com>
2016-11-28 16:10:43 -0800

    Add tests around scene.scene(Path|Name)
    I didn't bother trying to figure out good tests around scene.scenePath
    when there isn't a path included in the rename call, as it defaults to
    the project directory, but the commands that are supposed to give to me
    (cmds.workspace, e.g.) are giving me the directory that holds the
    projects, but not the actual, 'default' project directory that I need.

--- fmaya/core/test/test_scene.py
+++ fmaya/core/test/test_scene.py
@@ -1,8 +1,15 @@
 import unittest
 from nose.plugins.attrib import attr
 
+try:
+    import maya.cmds as cmds
+except ImportError:
+    print 'WARNING (%s): failed to load maya.cmds module.' % __file__
+
 from .. import scene
 
+
+# PURE
 
 class Test_toValidMayaName (unittest.TestCase):
 
@@ -23,4 +30,24 @@ class Test_toValidMayaName (unittest.TestCase):
 
     def test_toValidMayaName_replacesManyThings (self):
         self.assertEquals(scene.toValidMayaName("  This 'is' a test!"), "__This__is__a_test_")
+
+
+# IMPURE
+
+@attr('maya')
+class Test_scenePath (unittest.TestCase):
+
+    def setUp (self):
+        cmds.file(new=True, force=True)
+
+    def test_scenePath_getsScenePath (self):
+        testPath = "/tmp/testMayaScene.ma"
+        cmds.file(rename=testPath)
+        self.assertEquals(scene.scenePath(), testPath)
+
+    def test_sceneName_getsSceneName (self):
+        testPath = "/tmp/testMayaScene.ma"
+        testName = "testMayaScene.ma"
+        cmds.file(rename=testPath)
+        self.assertEquals(scene.sceneName(), testName)
 
