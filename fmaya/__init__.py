f15af51f8928b2f76c18bfc8e1c861e9435c2873
gfixler <gary@garyfixler.com>
2017-01-10 17:53:18 -0800

    Add node.py import in __init__.py

--- fmaya/__init__.py
+++ fmaya/__init__.py
@@ -2,6 +2,7 @@ from .core.pure import *
 from .core.chan import *
 from .core.hier import *
 from .core.name import *
+from .core.node import *
 from .core.scene import *
 from .core.select import *
 
