105b5806ba57c26c257ffb9af6e60e143dd123ff
gfixler <gary@garyfixler.com>
2019-06-19 19:11:36 -0700

    Find controls in some state, and juxt in tuples

---
+++ fmaya/archive/2019-06-19_04_advancedControlStateQueryingAndTupleJuxting.py
@@ -0,0 +1,14 @@
+ctrlState = lambda prop: lambda ctrl: cmds.control(ctrl, query=True, **{prop: True})
+filter(lambda x: cmds.button(x, query=True, enable=True), cmds.lsUI(type="button"))
+any(strict(cmap(ctrlState("enable"))(["button321", "button322", "button323"])))
+
+attr = lambda a: lambda x: cmds.getAttr(x + "." + a)
+
+pair = lambda f: lambda g: lambda x: (f(x), g(x))
+three = lambda f: lambda g: lambda h: lambda x: (f(x), g(x), h(x))
+ident = lambda x: x
+comp2 = lambda f: lambda g: lambda x: f(g(x))
+txyz = three(attr("tx"))(attr("ty"))(attr("tz"))
+
+data = strict(cmap(pair(ident)(txyz))(["persp","top","front","side"]))
+
