d147116e0c7296fa4bdf190df1d02144b37c0bea
gfixler <gary@garyfixler.com>
2021-04-15 07:43:38 -0700

    Fix var name typo in archived script

--- fmaya/archive/2019-06-21_02_toMayaName.py
+++ fmaya/archive/2019-06-21_02_toMayaName.py
@@ -1,7 +1,7 @@
 import string
 
 mayaNameHeadChars = '_' + string.ascii_letters
-mayaNameTailChars = mayaHeadChars + string.digits
+mayaNameTailChars = mayaNameHeadChars + string.digits
 toMayaNameHead = lambda c: c if c in mayaNameHeadChars else '_'
 toMayaNameTail = lambda c: c if c in mayaNameTailChars else '_'
 toMayaName = lambda n: ''.join([toMayaNameHead(n[0])] + map(toMayaNameTail, n[1:]))
